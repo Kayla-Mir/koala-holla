bf9f946381d0e35724860e3a9ebe81674bb08548
Michael Huso <85122010+husomichael@users.noreply.github.com>
2021-11-18 16:04:18 -0600

    Update client.js
    Made the empty TD

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -62,6 +62,7 @@ function getKoalas() {
           <td>${koala.gender}</td>
           <td>${koala.ready_to_transfer}</td>
           <td>${koala.notes}</td>
+          <td></td>
           <td><button class="deleteBtn" data-id="${koala.id}">Delete</button></td>
         </tr>
       `)
